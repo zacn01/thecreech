30c54c290ab5d644519b95a4e1094ee80126a780
Matthew Caldwell <mcaldwell@derbywars.com>
2017-10-21 09:02:57 -0400

    Add initial localization file

--- src/main/java/com/mcbeasts/item/ModItems.java
+++ src/main/java/com/mcbeasts/item/ModItems.java
@@ -5,8 +5,8 @@ import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
-	
-	public static ItemBase beastClaw = new ItemBase("beast_claw").setCreativeTab(CreativeTabs.MATERIALS);
+
+	public static ItemBase beastClaw = new ItemBase("beast_claw").setCreativeTab(CreativeTabs.MISC);
 
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.registerAll(
